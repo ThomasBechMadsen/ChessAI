d41b8dccb96b770dae1383a73b98c627c18caf00
eggman1979 <kim.d.rasmussen@live.dk>
2017-12-08 02:01:55 +0100

    rettet en fejl i king og movegenerator

--- src/logic/MoveGenerator.java
+++ src/logic/MoveGenerator.java
@@ -6,6 +6,7 @@ import java.util.List;
 import Utility.Position;
 import controller.BoardController;
 import game.Board;
+import pieces.King;
 import pieces.Piece;
 
 /**
@@ -27,13 +28,20 @@ public class MoveGenerator {
 	public static  ArrayList<Move> generateMoves(boolean isWhite, Board b){
 		ArrayList<Move> moves = new ArrayList<>();
 		Piece[][] board = b.getBoard();
-		
-		if((isWhite && b.isWhiteCheck) || (!isWhite && b.isBlackCheck)){
-		     moves = avoidCheckMoves(isWhite, b);
-		     return moves;
+
+		boolean[][] threat;
+		if(isWhite){
+			threat = b.blackThreat;
+		}else{
+			threat = b.whiteThreat;
 		}
-			
-		
+
+		if((isWhite && b.isWhiteCheck) || (!isWhite && b.isBlackCheck)){
+			moves = avoidCheckMoves(isWhite, b);
+			return moves;
+		}
+
+
 
 		// Går igennem brættet og finder brikkerne der måtte være.
 		for(int i = 0; i < 8; i++){
@@ -47,20 +55,29 @@ public class MoveGenerator {
 					if(piece.isWhite == isWhite){
 						// Der gåes igennem de mulige træk for brikken
 						for(Position pos: piece.possibleMoves(new Position(j,i),b)){
-							//	System.out.println("MoveGenerator brik farve check: " + piece.isWhite);
+
+
 							//Der bliver genereret et move
 							Piece targetPiece = board[pos.x][pos.y];
 
 							Position to = new Position(pos.x, pos.y);
 							Position from = new Position(j,i);
 							Move move = new Move(from, to, piece, targetPiece);
-//							//		System.out.println("MoveGenerator " + move);
-							
+							//							//		System.out.println("MoveGenerator " + move);
+
+
+
 							// move ordering
 							if(targetPiece != null  ){
 								moves.add(0, move);
 							}else{
 								moves.add(move);
+							}
+
+							if(piece instanceof King){
+								if(threat[pos.x][pos.y]){
+									moves.remove(move);
+								}
 							}
 						}
 					}
@@ -84,7 +101,7 @@ public class MoveGenerator {
 			Move move = new Move(kingPos, pos, king, b.getPieceAt(pos.x, pos.y));
 			moves.add(move);
 		}
-		
+
 		return moves;
 	}
 }
--- src/pieces/King.java
+++ src/pieces/King.java
@@ -43,22 +43,24 @@ public class King extends Piece{
 
 		Position[] positions = getPositions(pos);
 
+
+
 		for (Position position : positions) {
-
-
 			if(position.x >= 0 && position.x <8 && position.y >=0 && position.y <7){ // Check for out of boundt
-				if(!threat[position.x][position.y]){ // Hvis feltet er truet kan kongen ikke være der
+			
 					Piece p = board.getPieceAt(position.x, position.y); // gemmer feltet 
-
 					if(p != null){ // hvis feltet ikke er tom, og det er modstander brik, så tilføj
 						if(p.isWhite != isWhite){
+
 							moves.add(position);
+
 						}
 					}else{
+
 						moves.add(position); // hvis det er tomt, så tilføj det
 					}
 				}
-			}
+			
 		}
 		return moves;
 	}
@@ -88,8 +90,16 @@ public class King extends Piece{
 	}
 
 	public void detectThreat(Position pos, Board board){
-		
+
 		ArrayList<Position> sources = new ArrayList<>();
+
+		Position kingPos;
+		if(isWhite){
+			kingPos = board.whiteKing;
+		}
+		else{
+			kingPos = board.blackKing;
+		}
 
 		for(int y = 0 ; y < 8; y++){
 			for (int x = 0; x < 8; x++) {
@@ -100,10 +110,14 @@ public class King extends Piece{
 				}
 			}
 		}
+
+
 		for (Position position : sources) {
-			//if(position.equals( ))
+			if(position.equals(kingPos)){
+
+			}
 		}
-	
+
 	}
 	public Position[] getPositions(Position pos){
 		Position downRight = new Position(pos.x-1, pos.y-1);
