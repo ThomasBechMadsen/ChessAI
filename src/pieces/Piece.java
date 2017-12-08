84d8123d94270d43559ae5106740c150957fd9fd
Thomas Bech Madsen <thomasbm@live.dk>
2017-12-08 04:03:08 +0100

    Overwrite?

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -2,56 +2,30 @@ package pieces;
 import java.util.ArrayList;
 
 import Utility.Position;
-import game.Board;
+import dataContainers.Board;
 
 public abstract class Piece {
 	public boolean isWhite;
-	protected int baseValue;
-	protected int[][] positionalValue;
 	public int value;
-	//public int currentX;
-	//public int currentY;
-	public Type type;
 	public boolean hasAtLeastOneMove;
 	
 	public Piece(boolean friendly){
 		this.isWhite = friendly;
 	}
 	
-	public int getBaseValue(){
-		return baseValue;
-	}
+	public abstract int getBaseValue();
 	
-	public int getPositionalValue(int x, int y){
-		/**try{
-			positionalValue[x][y] = 3;
-			positionalValue[7-x][7-y] = 3;
-		}catch(ArrayIndexOutOfBoundsException e){
-			System.out.println(this.type);
-			return 3;
-		}*/
+	public abstract int getPositionalValue(int x, int y);
+	/*{
 		
 		if(isWhite){
 			return positionalValue[7-x][7-y]; //Should give the opposite positional value
 		}
 		
 		return positionalValue[x][y];
-	}
+	}*/
+
+	public abstract ArrayList<Position> possibleMoves(Position pos, Board board);
 	
-	public void setPositionalValue(int[][] value){
-		this.positionalValue = value;
-	}
-	
-	public Type getType(){
-		return type;
-	}
-	
-	
-	public ArrayList<Position> possibleMoves(Position pos, Board board){
-		return new ArrayList<Position>();
-	}
-	
-	public void calculateThreat(Position pos, Board board){
-		
-	}
+	public abstract void calculateThreat(Position pos, Board board);
 }
